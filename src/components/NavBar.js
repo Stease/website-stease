061577ae583d6768fabe9cce73cef12f55daf302
Stease <steasewebsites@gmail.com>
2021-02-15 12:59:47 +0100

    Added logo support

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { navbar } from '../preferences/preferences.js';
 // eslint-disable-next-line
 import styles from "../styling/navbar.css";
+import logo from "../img/logo.svg";
 
 export class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      companyName: navbar.companyName,
+      companyLogo: navbar.companyLogo,
       navbarItems: navbar.navbarItems,
       navbarItemsIcons: navbar.navbarItemsIcons,
       navbarLinks: navbar.navbarLinks,
@@ -17,7 +18,7 @@ export class NavBar extends React.Component {
   render() {
     return (
       <ul className="ul">
-        <li className="liLeft" key={0}><a className="aBold" href="?page=home">{this.state.companyName}</a></li>
+        <li className="liLeft" key={0}><img className="logo" src={logo} alt="logo" /></li>
         {this.state.navbarItems.map((item, i) =>
           <li className="liRight" key={i + 1}><a className="a" href={this.state.navbarLinks[i]}>{this.state.navbarItemsIcons[i]} {item}</a></li>
         )}
